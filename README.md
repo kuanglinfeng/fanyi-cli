59a92795d08b2330f2f570b80675edeeb7a021cc
Flinn <358530466@qq.com>
2020-04-06 01:20:07 +0800

    ok

---
+++ README.md
@@ -0,0 +1,17 @@
+## fanyi-cli
+
+一款基于nodejs的极简命令行翻译工具，使用TypeScript + Node.js实现。支持中英互译。
+
+## 用法
+
+1. 安装：npm install fanyi-cli -g
+
+2. 使用：
+
+f hello
+
+你好
+
+f 世界
+
+world
---
+++ src/main.ts
@@ -0,0 +1,84 @@
+import * as https from 'https'
+import * as querystring from 'querystring'
+import md5 = require('md5')
+import { appId, appSecret } from './private'
+
+type ErrorMap = {
+  [k: string]: string
+}
+
+const errorMap: ErrorMap = {
+  52001: '请求超时',
+  52002: '系统错误',
+  52003: '用户未授权',
+  54000: '必填参数为空',
+  54001: '签名错误',
+  54003: '访问频率受限',
+  54004: '账户余额不足',
+  54005: '长query请求频繁',
+  58000: '客户端IP非法',
+  58001: '译文语言方向不支持',
+  58002: '服务当前已经关闭',
+  90107: '认证未通过或未生效'
+}
+
+export const translate = (word: string) => {
+
+  const salt = Math.random()
+  const sign = md5(appId + word + salt + appSecret)
+  let from, to
+
+  if (/[a-zA-Z]/.test(word[0])) {
+    // 英译中
+    from = 'en'
+    to = 'zh'
+  } else {
+    // 中译英
+    from = 'zh'
+    to = 'en'
+  }
+
+  const query: string = querystring.stringify({
+    q: word,
+    appid: appId,
+    from, to, salt, sign
+  })
+
+  const options = {
+    hostname: 'api.fanyi.baidu.com',
+    port: 443,
+    path: '/api/trans/vip/translate?' + query,
+    method: 'GET'
+  }
+
+  type BaiduResult = {
+    error_code?: string,
+    error_msg?: string,
+    from: string,
+    to: string,
+    trans_result: { src: string, dst: string }[]
+  }
+
+  const request = https.request(options, (response) => {
+    const chunks: Buffer[] = []
+    response.on('data', (chunk: Buffer) => {
+      chunks.push(chunk)
+    })
+    response.on('end', () => {
+      const string = Buffer.concat(chunks).toString()
+      const object: BaiduResult = JSON.parse(string)
+      if (object.error_code) {
+        console.error(errorMap[object.error_code] || object.error_msg)
+        process.exit(2)
+      } else {
+        object.trans_result.forEach(obj => console.log(obj.dst))
+        process.exit(0)
+      }
+    })
+  })
+
+  request.on('error', (e) => {
+    console.log(e)
+  })
+  request.end()
+}
